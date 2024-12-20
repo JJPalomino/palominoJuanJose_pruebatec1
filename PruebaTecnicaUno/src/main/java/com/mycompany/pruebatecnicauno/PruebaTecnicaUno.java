419c63c958abd42a6840827c6c9ce82ecd63448e
Juan José Palomino Jacobo <josepalomino_@outlook.com>
2024-12-20 16:23:09 -0600

    Creación del Proyecto

---
+++ PruebaTecnicaUno/src/main/java/com/mycompany/pruebatecnicauno/PruebaTecnicaUno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebatecnicauno;
+
+/**
+ *
+ * @author Palomino
+ */
+public class PruebaTecnicaUno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
